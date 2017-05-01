ffbf0576d8190199afd5537554c9e6a617255f45
KEVINXPS15\kevin <Kevin.vandenbroek@Home.nl>
2017-05-01 09:53:26 +0200

    FIX OpenGL BoilerPlate

--- MetroGame/MetroGame/Main.cpp
+++ MetroGame/MetroGame/Main.cpp
@@ -3,18 +3,38 @@
 #include <iostream>
 using namespace std; //Lets not do this
 
+int width = 1920;
+int height = 1080;
+
+/*
+*	Display function, Do the main rendering here.
+*/
+void display() {
+
+}
+
+/*
+*	Idle function.
+*/
+void idle() {
+
+}
+
+
 int main(int argc, char* argv[])
 {
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(width, height);
 	glutInit(&argc, argv);
-	glutCreateWindow("Hello OpenGL");
+	glutCreateWindow("MetroGame");
 	glutDisplayFunc(display);
 	glutIdleFunc(idle);
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(keyboardSpecial);
-	glutMotionFunc(changeColor);
+	
+	/*	glutReshapeFunc(reshape);
+		glutKeyboardFunc(keyboard);
+		glutSpecialFunc(keyboardSpecial);
+		glutMotionFunc(changeColor);
+	*/
 	//opengl init
 
 	glEnable(GL_DEPTH_TEST);
