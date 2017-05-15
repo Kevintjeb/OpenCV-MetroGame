5b0c5a064c73eaf98cf5ddfe5bdd2334eda64fb1
swavans <sworms@avans.nl>
2017-05-15 09:07:05 +0200

    Fixed Test

--- MetroGame/MetroGame/Test.h
+++ MetroGame/MetroGame/Test.h
@@ -1,15 +1,14 @@
 #include <list>
 #include "Vect.h"
-/*
+
 namespace mg_gameLogic
 {
 	class Test {
 	public: 
 		Test();
 		~Test();
-		MetroLine metro;
+		Line metro;
 
 		void testMetroLine();
 	};
 }
-*/
