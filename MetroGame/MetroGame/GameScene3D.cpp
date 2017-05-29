2ba69f7f5e87754dd5fc4be919c8f5cf93a65788
RickvF <pajfesse@avans.nl>
2017-05-29 14:01:38 +0200

    ADD GameScene3D prefab

---
+++ MetroGame/MetroGame/GameScene3D.cpp
@@ -0,0 +1,53 @@
+#include "GameScene3D.h"
+
+
+
+
+GameScene3D::GameScene3D()
+{
+}
+
+
+GameScene3D::~GameScene3D()
+{
+}
+
+void GameScene3D::render()
+{
+}
+
+void GameScene3D::update()
+{
+}
+
+void GameScene3D::onEnter()
+{
+}
+
+void GameScene3D::onExit()
+{
+}
+
+void GameScene3D::onKeyUP(unsigned char)
+{
+}
+
+void GameScene3D::onKeyDown(unsigned char)
+{
+}
+
+void GameScene3D::onIdle()
+{
+}
+
+void GameScene3D::onSpecialFunc(int)
+{
+}
+
+void GameScene3D::onSpecialUpFunc(int)
+{
+}
+
+void GameScene3D::reshapeFunc(int, int)
+{
+}
