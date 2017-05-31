9b4f386da1b0c2063ceca91d79373f1c2b2ae73b
RickvF <pajfesse@avans.nl>
2017-05-31 11:50:07 +0200

    FIX flobos request why are all the Glut interface functions public? since Glut should be the only one to call these functions. The helper functions are also public why?

--- MetroGame/MetroGame/SceneManager.h
+++ MetroGame/MetroGame/SceneManager.h
@@ -9,18 +9,8 @@ class SceneManager
 public:
 	void loadScene(IScene *);
 	IScene& getCurrentScene();
-	void render3D();
-	void render2D();
 	int getWidth();
 	int getHeight();
-	void createWindow(int, int, std::string, void(* callback)());
-	void tick();
-	void onKeyUp(unsigned char);
-	void onKeyDown(unsigned char);
-	void onIdle();
-	void onSpecialFunc(int key);
-	void onSpecialUpFunc(int key);
-	void reshapeFunc(int, int);
 	void init();
 	static SceneManager& getInstance();
 
@@ -31,6 +21,16 @@ private:
 	SceneManager(const SceneManager&);
 	SceneManager();
 	~SceneManager();
+	void onKeyUp(unsigned char);
+	void onKeyDown(unsigned char);
+	void onIdle();
+	void onSpecialFunc(int key);
+	void onSpecialUpFunc(int key);
+	void reshapeFunc(int, int);
+	void createWindow(int, int, std::string, void(*callback)());
+	void render3D();
+	void render2D();
+	void tick();
 	IScene* currentScene;
 };
 
