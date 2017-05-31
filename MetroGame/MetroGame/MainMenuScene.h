dbe21bfedaac3f2536a51133ec7d495ad01ef971
theRealFlobo <florisbobvanelzelingen@gmail.com>
2017-05-31 15:18:04 +0200

    FIX removed the virtual keyword from IScene implementations according to Pull request comment from myself

--- MetroGame/MetroGame/MainMenuScene.h
+++ MetroGame/MetroGame/MainMenuScene.h
@@ -16,16 +16,16 @@ public:
 	~MainMenuScene();
 	
 	// Inherited via IScene
-	virtual void render3D() override;
-	virtual void render2D() override;
-	virtual void onEnter() override;
-	virtual void onExit() override;
-	virtual void update() override;
-	virtual void onIdle() override;
-	virtual void onKeyUP(unsigned char) override;
-	virtual void onKeyDown(unsigned char) override;
-	virtual void onSpecialFunc(int) override;
-	virtual void onSpecialUpFunc(int) override;
-	virtual void reshapeFunc(int, int) override;
+	void render3D() override;
+	void render2D() override;
+	void onEnter() override;
+	void onExit() override;
+	void update() override;
+	void onIdle() override;
+	void onKeyUP(unsigned char) override;
+	void onKeyDown(unsigned char) override;
+	void onSpecialFunc(int) override;
+	void onSpecialUpFunc(int) override;
+	void reshapeFunc(int, int) override;
 };
 
