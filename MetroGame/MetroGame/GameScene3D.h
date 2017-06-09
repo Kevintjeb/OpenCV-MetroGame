6cd8592a91a60d0e8a1b04c4337feb8d8f0a06ec
KEVINXPS15\kevin <Kevin.vandenbroek@Home.nl>
2017-06-09 08:58:23 +0200

    FIX Rick his C code

--- MetroGame/MetroGame/GameScene3D.h
+++ MetroGame/MetroGame/GameScene3D.h
@@ -38,13 +38,92 @@ class GameScene3D :
 	public IScene
 {
 private:
+
+	float randScale(float standScale);
+	void createCityList();
+	void initGroundPlane();
+	void drawVertexArray(std::vector<VertexClass> verts, GLenum mode, int start, int end);
+	void prepareModel(std::string modelPath);
+	void move(float angle, float fac);
+	void drawRenderables();
+	void draw2DRenderables();
+	Vec3f getLineColor(LineType color);
+	void prepare_lines();
+	void prepare_lines2D();
+	ModelLoader modelLoader;
+	void setAllKeysFalse();
+
+	std::map<std::string, int> modelsMap;
+	std::map<std::string, int>::iterator it;
+
+	GLuint window_db;
+
+	LinePointer handle;
+	Renderable testTrain;
+	Line *line;
+	MetroTrain *train;
+
+	LinePointer handle2;
+	Line *line2;
+	MetroTrain *train2;
+
+	LinePointer handle3;
+	Line *line3;
+	MetroTrain *train3;
+
+	LinePointer handle4;
+	Line *line4;
+	MetroTrain *train4;
+
+	LinePointer handle5;
+	Line *line5;
+	MetroTrain *train5;
+
+	std::vector <VertexClass> TopPlane;
+	std::vector <VertexClass> GroundPlane;
+	std::vector <VertexClass> metroLines;
+	std::vector <std::pair<int, int>> metroLinesPosition;
+	std::vector <VertexClass> metroLines2D;
+	std::vector <std::pair<int, int>> metroLinesPosition2D;
+	std::vector<Texture*> textures;
+	std::vector<RenderablePointer> renderablePointers;
+	std::vector<Passengers> passengers;
+
+	int oldTime = -1;
+
+	int lastTime;
+	float rotation;
+
+	int speed = 25;
+	bool keys[255];
+	bool shiftActive = false;
+
+	struct Camera
+	{
+		float posX = 7.57;
+		float posY = -103.79;
+		float rotX = 0;
+		float rotY = 0;
+		float height = 41.07;
+	} camera;
+
+	struct Camera2D
+	{
+		float posX = 0;
+		float posY = 0;
+		float rotX = 90;
+		float rotY = 0;
+		float height = -80;
+	}camera2D;
+
 	static Font* largeFont3D;
 
-	string time;
+	string timeString;
 	struct {
 		float seconds = 0;
 		int minutes = 0;
 	}Time;
+
 	void prepareTime(float deltaTime);
 
 public:
